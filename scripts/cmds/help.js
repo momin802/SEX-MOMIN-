1481c0b93dc457cae3443d46b05a98bbe1d7e669
‎⎯᪵⎯꯭̽𝆺𝅥𓆩ᴄʀɪᴍɪɴᴀʟ_🅜𝐎𝐌🅘𝐍𓆪꯭)꯭⟶᯦꯭🍅🧃 <mominjannat567@gmail.com>
2026-05-08 01:44:34 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -7,7 +7,7 @@ let xfont = null;
 let yfont = null;
 let categoryEmoji = null;
 
-const HELP_GIF = "https://files.catbox.moe/6touzq.mp4";
+const HELP_GIF = "https://files.catbox.moe/8srwzu.mp4";
 
 // 🔒 AUTHOR LOCK SYSTEM
 const AUTHOR_NAME = "FARHAN-KHAN";
